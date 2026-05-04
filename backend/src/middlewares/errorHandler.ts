06a89229db76ea10764982582c9f970f036ac9bd
Maria Eliseeva <marymaryeliseeva@yandex.ru>
2026-05-04 09:56:32 +0300

    добавлен nextFunction в сигнатуру errorHandler

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -1,9 +1,9 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import BadRequestError from '../errors/bad-request-error';
 import ConflictError from '../errors/conflict-error';
 import NotFoundError from '../errors/not-found-error';
 
-const errorHandler = (err: Error, _req: Request, res: Response) => {
+const errorHandler = (err: Error, _req: Request, res: Response, _next: NextFunction) => {
   if (
     err instanceof BadRequestError
     || err instanceof ConflictError
